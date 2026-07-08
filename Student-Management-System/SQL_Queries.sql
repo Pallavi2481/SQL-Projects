266a2dcdfc18412bb74c1b4f58f1111045c48a47
Pallavi2481 <95669380+Pallavi2481@users.noreply.github.com>
2026-07-08 10:10:29 +0530

    Update SQL_Queries.sql

--- Student-Management-System/SQL_Queries.sql
+++ Student-Management-System/SQL_Queries.sql
@@ -5,3 +5,18 @@ SHOW DATABASES;
 USE StudentDB;
 
 SHOW TABLES;
+SHOW TABLES;
+
+SELECT * FROM Department;
+
+SELECT * FROM Student;
+
+SELECT * FROM Faculty;
+
+SELECT * FROM Subject;
+
+SELECT * FROM Course;
+
+SELECT * FROM Attendance;
+
+SELECT * FROM Marks;

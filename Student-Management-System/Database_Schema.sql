04c6e9dca97545b6a97ea411b1a9fb85f040eed8
Pallavi2481 <95669380+Pallavi2481@users.noreply.github.com>
2026-07-08 10:14:48 +0530

    Update Database_Schema.sql

--- Student-Management-System/Database_Schema.sql
+++ Student-Management-System/Database_Schema.sql
@@ -3,6 +3,33 @@
 CREATE DATABASE StudentDB;
 
 USE StudentDB;
+CREATE TABLE Department (
+    Department_ID INT PRIMARY KEY,
+    Department_Name VARCHAR(50),
+    HOD VARCHAR(50)
+);
+CREATE TABLE Student (
+    Student_ID INT PRIMARY KEY,
+    Student_Name VARCHAR(50),
+    Gender VARCHAR(10),
+    Department_ID INT,
+    FOREIGN KEY (Department_ID)
+    REFERENCES Department(Department_ID)
+);
+CREATE TABLE Faculty (
+    Faculty_ID INT PRIMARY KEY,
+    Faculty_Name VARCHAR(50),
+    Department_ID INT,
+    FOREIGN KEY (Department_ID)
+    REFERENCES Department(Department_ID)
+);
+CREATE TABLE Subject (
+    Subject_ID INT PRIMARY KEY,
+    Subject_Name VARCHAR(50),
+    Faculty_ID INT,
+    FOREIGN KEY (Faculty_ID)
+    REFERENCES Faculty(Faculty_ID)
+);
 CREATE TABLE Course (
     Course_ID INT PRIMARY KEY,
     Course_Name VARCHAR(50),

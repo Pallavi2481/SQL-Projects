122bddd607cd6e7e9790a36285831f179f1444c9
Pallavi2481 <95669380+Pallavi2481@users.noreply.github.com>
2026-07-08 10:09:28 +0530

    Update Database_Schema.sql

--- Student-Management-System/Database_Schema.sql
+++ Student-Management-System/Database_Schema.sql
@@ -3,3 +3,28 @@
 CREATE DATABASE StudentDB;
 
 USE StudentDB;
+CREATE TABLE Course (
+    Course_ID INT PRIMARY KEY,
+    Course_Name VARCHAR(50),
+    Duration VARCHAR(20)
+);
+
+CREATE TABLE Attendance (
+    Attendance_ID INT PRIMARY KEY,
+    Student_ID INT,
+    Attendance_Date DATE,
+    Status VARCHAR(10),
+    FOREIGN KEY (Student_ID)
+    REFERENCES Student(Student_ID)
+);
+
+CREATE TABLE Marks (
+    Mark_ID INT PRIMARY KEY,
+    Student_ID INT,
+    Subject_ID INT,
+    Marks INT,
+    FOREIGN KEY (Student_ID)
+    REFERENCES Student(Student_ID),
+    FOREIGN KEY (Subject_ID)
+    REFERENCES Subject(Subject_ID)
+);
